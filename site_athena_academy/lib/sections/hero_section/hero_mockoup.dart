eca5e2128aa4816bc85402e4666fb3ba6df1ebf1
Ian Page Miranda Maciel <179640858+ianpage08@users.noreply.github.com>
2025-12-09 00:25:24 -0300

    animation hero

--- site_athena_academy/lib/sections/hero_section/hero_mockoup.dart
+++ site_athena_academy/lib/sections/hero_section/hero_mockoup.dart
@@ -1,30 +1,67 @@
 import 'package:flutter/material.dart';
 
-class HeroMockup extends StatelessWidget {
+class HeroMockup extends StatefulWidget {
   const HeroMockup({super.key});
 
   @override
+  State<HeroMockup> createState() => _HeroMockupState();
+}
+
+class _HeroMockupState extends State<HeroMockup>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+  late Animation<double> _animationTime;
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 3),
+    )..repeat(reverse: true);
+
+    _animationTime = Tween(begin: 0.0, end: -8.0).animate(
+      CurvedAnimation(parent: _animationController, curve: Curves.easeInOut),
+    );
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: const BoxDecoration(
-        boxShadow: [
-          // sombra profunda
-          BoxShadow(
-            color: Color.fromARGB(80, 0, 0, 0),
-            blurRadius: 90,
-            offset: Offset(0, 50),
-          ),
-          // glow branco suave
-          BoxShadow(
-            color: Color.fromARGB(40, 255, 255, 255),
-            blurRadius: 140,
-            offset: Offset(0, 20),
-          ),
-        ],
-      ),
-      child: Padding(
-        padding: const EdgeInsets.only(top: 20),
-        child: Image.asset('assets/imagens/hero_1.png', width: 600),
+    return AnimatedBuilder(
+      animation: _animationController,
+      builder: (context, child) {
+        return Transform.translate(
+          offset: Offset(0, _animationTime.value),
+          child: child,
+        );
+      },
+      child: Container(
+        decoration: const BoxDecoration(
+          boxShadow: [
+            // sombra profunda
+            BoxShadow(
+              color: Color.fromARGB(80, 0, 0, 0),
+              blurRadius: 90,
+              offset: Offset(0, 50),
+            ),
+            // glow branco suave
+            BoxShadow(
+              color: Color.fromARGB(40, 255, 255, 255),
+              blurRadius: 140,
+              offset: Offset(0, 20),
+            ),
+          ],
+        ),
+        child: Padding(
+          padding: const EdgeInsets.only(top: 20),
+          child: Image.asset('assets/imagens/hero_1.png', width: 600),
+        ),
       ),
     );
   }
--- site_athena_academy/lib/sections/hero_section/hero_text.dart
+++ site_athena_academy/lib/sections/hero_section/hero_text.dart
@@ -1,55 +1,120 @@
 import 'package:flutter/material.dart';
-import 'package:site_athena_academy/sections/hero_section/hero_buttons.dart';
+import 'package:site_athena_academy/core/constants/colors.dart';
 import 'package:site_athena_academy/theme/text_style.dart';
+import 'package:site_athena_academy/widgets/button_ishovered.dart';
 
-class HeroText extends StatelessWidget {
+class HeroText extends StatefulWidget {
   const HeroText({super.key});
 
   @override
+  State<HeroText> createState() => _HeroTextState();
+}
+
+class _HeroTextState extends State<HeroText>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+  late Animation<double> _animationSlide;
+  late Animation<double> _animationOpacity;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _animationController = AnimationController(
+      duration: const Duration(seconds: 3),
+      vsync: this,
+    )..forward();
+
+    _animationOpacity = CurvedAnimation(
+      parent: _animationController,
+      curve: Curves.easeOut,
+    );
+
+    _animationSlide = Tween(begin: 20.0, end: 0.0).animate(
+      CurvedAnimation(parent: _animationController, curve: Curves.easeOutCubic),
+    );
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: [
-        Column(
-          children: [
-            Container(
-              decoration: const BoxDecoration(
-                boxShadow: [
-                  BoxShadow(
-                    blurRadius: 22,
-                    spreadRadius: -4,
-                    color: Color.fromARGB(22, 255, 255, 255),
-                  ),
-                ],
+    return AnimatedBuilder(
+      animation: _animationController,
+      builder: (_, child) {
+        return Opacity(
+          opacity: _animationOpacity.value,
+          child: Transform.translate(
+            offset: Offset(0, _animationSlide.value),
+            child: child,
+          ),
+        );
+      },
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Column(
+            children: [
+              Container(
+                decoration: const BoxDecoration(
+                  boxShadow: [
+                    BoxShadow(
+                      blurRadius: 22,
+                      spreadRadius: -4,
+                      color: Color.fromARGB(22, 255, 255, 255),
+                    ),
+                  ],
+                ),
+                child: Image.asset('assets/imagens/logo_white.png', width: 140),
               ),
-              child: Image.asset('assets/imagens/logo_white.png', width: 140),
-            ),
-            const SizedBox(height: 16),
-            Text('ATHENA ACADEMY', style: AthenaTextStyle.display),
-          ],
-        ),
+              const SizedBox(height: 16),
+              Text('ATHENA ACADEMY', style: AthenaTextStyle.display),
+            ],
+          ),
 
-        const SizedBox(height: 32),
+          const SizedBox(height: 32),
 
-        Text(
-          'A plataforma educacional do futuro',
-          textAlign: TextAlign.center,
-          style: AthenaTextStyle.subtitle,
-        ),
+          Text(
+            'A plataforma educacional do futuro',
+            textAlign: TextAlign.center,
+            style: AthenaTextStyle.subtitle,
+          ),
 
-        const SizedBox(height: 6),
+          const SizedBox(height: 6),
 
-        Text(
-          'Uma plataforma moderna, intuitiva e feita para aproximar escolas,\n'
-          'professores, alunos e pais.',
-          textAlign: TextAlign.center,
-          style: AthenaTextStyle.small,
-        ),
+          Text(
+            'Uma plataforma moderna, intuitiva e feita para aproximar escolas,\n'
+            'professores, alunos e pais.',
+            textAlign: TextAlign.center,
+            style: AthenaTextStyle.small,
+          ),
 
-        const SizedBox(height: 24),
+          const SizedBox(height: 24),
 
-        const HeroButtons(),
-      ],
+          const Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              AthenaButton(
+                label: 'Baixar App Demo',
+                icon: Icons.download,
+                backgroundColor: AthenaColors.accent,
+                foregroundColor: Color.fromARGB(255, 255, 255, 255),
+              ),
+              SizedBox(width: 16),
+              AthenaButton(
+                label: 'Explorar Repositorio',
+                icon: Icons.dock,
+                backgroundColor: Colors.white,
+                foregroundColor: Colors.black,
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
